4ca48720570b0e7069e7a1dde7c5db3655900563
Leandro Coutinho <lbcoutinho.blm@gmail.com>
2018-05-31 22:14:28 -0300

    SendGrid Webhook and Surveys Dashboard

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,14 +1,77 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
+
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
 const mongoose = require('mongoose');
 const Survey = mongoose.model('surveys');
+
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    // Get all survey from user and select fields to return (In Mongo it's called "projection")
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+    res.send(surveys);
+  });
+
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+  // SendGrid may take a long time to send a response in development env
+  app.post('/api/surveys/webhooks', (req, res) => {
+    // Pattern to extract surveyId and choice from route
+    const pathPattern = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        // Get route from URL = /api/surveys/123/yes
+        const { pathname } = new URL(url);
+        // .test returns an object containing surveyId and choice from route
+        const match = pathPattern.test(pathname);
+        // If pattern not found match will be undefined
+        if (match) {
+          return { email, ...match };
+        }
+      })
+      // Remove undefined values from array
+      .compact()
+      // Remove duplicates by checking if objects have same email and surveyId
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          // Query object
+          {
+            // Find survey that matches  surveyId and exact recipient that matches email and responded = false
+            _id: surveyId,
+            recipients: {
+              $elemMatch: {
+                email: email,
+                responded: false
+              }
+            }
+          },
+          // Update object
+          {
+            // Find yes/no property and increment 1
+            $inc: { [choice]: 1 },
+            // Set responded = true in the exact recipient found in the query, this is indicated by the $ sign below
+            $set: {
+              'recipients.$.responded': true
+            },
+            lastResponded: new Date()
+          }
+        ).exec(); // exec() trigger operation in mongo
+      })
+      .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
@@ -36,8 +99,4 @@ module.exports = app => {
       res.status(422).send(err);
     }
   });
-
-  // app.get('/api/surveys', requireLogin, (req, res) => {});
-
-  // app.post('/api/surveys/webhooks', requireLogin, (req, res) => {});
 };
---
+++ server/sendgrid_webhook.js
@@ -0,0 +1,5 @@
+var localtunnel = require('localtunnel');
+
+localtunnel(5000, { subdomain: 'leotec8721dlkjaskldj1283nlkasd' }, function(err, tunnel) {
+  console.log('LT running');
+});
